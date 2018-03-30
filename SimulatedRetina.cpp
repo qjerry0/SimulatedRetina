4623e5f0ac7e59bb75c67d44210056f324fef8b0
tareyza <tareyza@yahoo.com>
2018-03-30 13:06:16 -0400

    Removed QT stuff in main

--- SimulatedRetina.cpp
+++ SimulatedRetina.cpp
@@ -9,12 +9,7 @@
 #include "Quadtree.h"
 #include "Image.cpp"
 #include <fstream>
-
-#include <QApplication>
-#include <QCommandLineParser>
-#include <QCommandLineOption>
-
-#include "mainwindow.h"
+#include "Data.h"
 
 int main(int argc, char *argv[])
 {
@@ -51,25 +46,6 @@ int main(int argc, char *argv[])
 	Image img("input.png");
 	img.write("output.png");
 	return 0;
-
-	Q_INIT_RESOURCE(application);
-
-	QApplication app(argc, argv);
-	QCoreApplication::setOrganizationName("QtProject");
-	QCoreApplication::setApplicationName("Application Example");
-	QCoreApplication::setApplicationVersion(QT_VERSION_STR);
-	QCommandLineParser parser;
-	parser.setApplicationDescription(QCoreApplication::applicationName());
-	parser.addHelpOption();
-	parser.addVersionOption();
-	parser.addPositionalArgument("file", "The file to open.");
-	parser.process(app);
-
-	MainWindow mainWin;
-	if (!parser.positionalArguments().isEmpty())
-		mainWin.loadFile(parser.positionalArguments().first());
-	mainWin.show();
-	return app.exec();
 
 }
 
